6f4f329b67319e7a0772129a424cad0768e02d81
Bharath Naik <108934537+Bharathnaik90@users.noreply.github.com>
2022-07-09 20:50:18 +0530

    Add files via upload

---
+++ my.py
@@ -0,0 +1,37 @@
+
+from colorama import Fore,Back,Style
+
+print(Back.WHITE + Fore.BLACK + Style.BRIGHT + "-------------------------simple calculeter------------------------" + Style.RESET_ALL + Fore.RESET + Back.RESET)
+
+print()
+
+print(Fore.YELLOW + "Opstion 1 = Addision" + Fore.RESET)
+print(Fore.YELLOW  + Style.DIM + "Opstion 2 = Substrasion" + Style.RESET_ALL + Fore.RESET)
+print(Fore.YELLOW + "Opstion 3 = Multifacsion" + Fore.RESET)
+print(Fore.YELLOW + "Opstion 4 = Division" + Fore.RESET)
+
+print()
+
+while True :
+	opstion = int(input(Fore.MAGENTA + 'Chose Your Opstion (1/2/3/4) >> ' + Fore.RESET))
+
+	print()
+
+	Num1 = int(input(Fore.RED + 'Type your number > ' + Fore.RESET))
+	Num2 = int(input(Fore.RED + 'Type your number > ' + Fore.RESET))
+
+	print()
+
+	if opstion == 1 :
+		print(Fore.GREEN + f'Answer = {Num1+Num2}' + Fore.RESET)
+	elif opstion == 2 :
+		print(Fore.GREEN + f'Answer = {Num1-Num2}' + Fore.RESET)
+	elif opstion == 3 :
+		print(Fore.GREEN + f'Answer = {Num1*Num2}' + Fore.RESET)
+	elif opstion == 4 :
+		print(Fore.GREEN + f'Answer = {Num1//Num2}' + Fore.RESET)
+	else :
+		print("That One Opstion Is Invild.....!!!! Please Chose - 1/2/3/4 ")
+	
+	print()
+	
